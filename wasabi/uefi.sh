2668f217a1ebed2571a97de84a6735cbf04aee50
qraqras <q100rrraaa@gmail.com>
2025-06-18 12:18:43 +0000

    p.68

--- wasabi/uefi.sh
+++ wasabi/uefi.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cargo  build --target x86_64-unknown-uefi
 cp target/x86_64-unknown-uefi/debug/wasabi.efi mnt/EFI/BOOT/BOOTX64.EFI
-qemu-system-x86_64 -bios third_party/ovmf/RELEASEX64_OVMF.fd -drive format=raw,file=fat:rw:mnt -nographic
+#qemu-system-x86_64 -bios third_party/ovmf/RELEASEX64_OVMF.fd -drive format=raw,file=fat:rw:mnt -nographic
+qemu-system-x86_64 -bios third_party/ovmf/RELEASEX64_OVMF.fd -drive format=raw,file=fat:rw:mnt -object secret,id=password,data=password -vnc :0,password-secret=password
